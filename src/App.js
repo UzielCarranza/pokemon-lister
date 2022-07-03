a42e853c2e67f46cef983d5c1872c5a5fd5c6e3e
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-07-03 17:40:52 -0500

    importing component to display a pokemon from the Pokemons API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import {Pokemons} from "./Components/Pokemons";
+import {getServerData} from "./Components/GetServerData";
+import {DataSource} from "./Components/DataSource";
 
 
 function App() {
-  return (
-    <div className="App">
-      <Pokemons/>
-    </div>
-  );
+    return (
+        <>
+            <DataSource getDataFunc={getServerData('https://pokeapi.co/api/v2/pokemon/ditto')} resourceName="pokemon" >
+                <Pokemons/>
+            </DataSource>
+        </>
+    )
 }
 
 export default App;
