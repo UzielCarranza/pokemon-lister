bc79b5cf11e7eb6f0d54ce2deaef8d3de440771a
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-07-03 17:40:08 -0500

    getting a 200 response from pokemon api

--- src/Components/Pokemons.js
+++ src/Components/Pokemons.js
@@ -1,13 +1,11 @@
-
-
-
 export const Pokemons = (props) => {
     console.log(props)
 
-    return (
+    return props ? (
         <div>
-            <h1>Pokemon's list</h1>
+            <h1>{props.pokemon.forms[0].name}</h1>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/132.png" alt="ditto"/>
         </div>
-    )
+    ) : ""
 
 }
