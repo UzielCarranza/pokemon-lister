73b4da48927c0f31bcfcc22591e457b0682cf759
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-07-04 11:46:11 -0500

    created component to display all pokemons and their ID's

--- src/Components/PokemonsForms.js
+++ src/Components/PokemonsForms.js
@@ -1,8 +1,19 @@
+import {useState, useEffect} from "react";
+export const PokemonsForms = (props) => {
+    const [forms, setForms] = useState(null);
 
 
-export const PokemonForms = (props) => {
-    console.log(props)
-    return (
-        <div>hello</div>
-    )
+    useEffect(() => {
+        if (props !== null) {
+            setForms(props.forms)
+            console.log(props)
+        }
+    }, [props])
+    return forms !== null ? (<>
+
+        <div>
+            <h1>hi {props.forms.pokemon.name}</h1>
+            <img src={props.forms.sprites.back_default} alt=""/>
+        </div>
+    </>) : ""
 }
