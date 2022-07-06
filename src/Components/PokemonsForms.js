8107d6831dd5e90a71cd9c1601a245221d89ec64
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-07-06 11:57:14 -0500

    added stats component to be render to the DOM, hiding abilities components for now... future pop up modal incorporation

--- src/Components/PokemonsForms.js
+++ src/Components/PokemonsForms.js
@@ -15,22 +15,18 @@ export const PokemonsForms = (props) => {
     return forms !== null ? (<>
 
         <div className="card">
-            <div className="card-front">
-                <h1 className="pokemon-name">{props.forms.pokemon.name}</h1>
-                <img src={props.forms.sprites.front_default} alt=""/>
-                <div className="card-back">
-                    <h3>{props.forms.types[0].type.name}</h3>
-                    {/*<DataSource getDataFunc={getServerData(`https://pokeapi.co/api/v2/pokemon/${props.forms.pokemon.name}/`)}*/}
-                    {/*            resourceName="abilities" >*/}
-                    {/*    <Abilities/>*/}
-                    {/*</DataSource>*/}
-                    <DataSource getDataFunc={getServerData(`https://pokeapi.co/api/v2/pokemon/${props.forms.pokemon.name}/`)}
-                                resourceName="stats" >
-                        <Stats/>
-                    </DataSource>
-                </div>
+            <h1 className="pokemon-name">{props.forms.pokemon.name}</h1>
+            <img className="card-img" src={props.forms.sprites.front_default} alt=""/>
 
-            </div>
+            <DataSource getDataFunc={getServerData(`https://pokeapi.co/api/v2/pokemon/${props.forms.pokemon.name}/`)}
+                        resourceName="stats">
+                <Stats/>
+            </DataSource>
+            <h3>{props.forms.types[0].type.name}</h3>
+            {/*<DataSource getDataFunc={getServerData(`https://pokeapi.co/api/v2/pokemon/${props.forms.pokemon.name}/`)}*/}
+            {/*            resourceName="abilities" >*/}
+            {/*    <Abilities/>*/}
+            {/*</DataSource>*/}
         </div>
 
 
