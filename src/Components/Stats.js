98ab96dca783258935d49d7bdad249fb3573a7a4
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-07-07 11:05:30 -0500

    divided every stats inside of a div for display requirements

--- src/Components/Stats.js
+++ src/Components/Stats.js
@@ -10,26 +10,43 @@ export const Stats = (props) => {
         }
     }, [props])
     return stats !== null ? (<>
-        <div className="stats-wrapper">
+    <section className="stats-wrapper">
+
+
+        <div className="stats">
             <h2>{stats.stats[0].stat.name}</h2>
-            <h5 className="stats">{stats.stats[0].base_stat}</h5>
+            <h5>{stats.stats[0].base_stat}</h5>
+    </div>
 
 
-            <h2>{stats.stats[1].stat.name}</h2>
-            <h5>{stats.stats[1].base_stat}</h5>
+    <div className="stats">
+        <h2>{stats.stats[1].stat.name}</h2>
+        <h5>{stats.stats[1].base_stat}</h5>
+    </div>
 
-            <h2>{stats.stats[2].stat.name}</h2>
-            <h5>{stats.stats[2].base_stat}</h5>
 
-            <h2>{stats.stats[3].stat.name}</h2>
-            <h5>{stats.stats[3].base_stat}</h5>
+    <div className="stats">
 
-            <h2>{stats.stats[4].stat.name}</h2>
-            <h5>{stats.stats[4].base_stat}</h5>
+        <h2>{stats.stats[2].stat.name}</h2>
+        <h5>{stats.stats[2].base_stat}</h5>
+    </div>
 
-            <h2>{stats.stats[5].stat.name}</h2>
-            <h5>{stats.stats[5].base_stat}</h5>
-        </div>
+    <div className="stats">
+        <h2>{stats.stats[3].stat.name}</h2>
+        <h5>{stats.stats[3].base_stat}</h5>
+    </div>
+    <div className="stats">
 
-    </>) : ""
+        <h2>{stats.stats[4].stat.name}</h2>
+        <h5>{stats.stats[4].base_stat}</h5>
+    </div>
+
+    <div className="stats">
+        <h2>{stats.stats[5].stat.name}</h2>
+        <h5>{stats.stats[5].base_stat}</h5>
+    </div>
+    </section>
+
+</>) :
+    ""
 }
