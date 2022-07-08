141433c16cb19665c787c4263616777df1a4ecd2
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-07-08 09:32:41 -0500

    added search pokemon by name functionality

--- src/Components/Pokemons.js
+++ src/Components/Pokemons.js
@@ -3,24 +3,75 @@ import {getServerData} from "../Utils/GetServerData";
 import {DataSource} from "../Utils/DataSource";
 import {PokemonsForms} from "./PokemonsForms";
 import './Pokemons.css';
+import {SiPokemon} from "react-icons/si";
 
 
 export const Pokemons = (props) => {
     const [pokemons, setPokemons] = useState(null);
+    const [searchValue, setSearchValue] = useState(null)
+    const [searchByName, setSearchByName] = useState("");
+
 
     useEffect(() => {
         if (props.pokemon !== null) {
             setPokemons(props.pokemon)
         }
     }, [props])
+
+
+    const search = () => {
+        const poke = pokemons.results.filter(value => value.name.toLowerCase().includes(searchByName.toLowerCase()))
+        setSearchValue(poke)
+    }
+
+
     return pokemons !== null ? (
         <>
-            {pokemons.results.map((item, i) => (
-                    <DataSource key={pokemons.results[i].name} getDataFunc={getServerData(`https://pokeapi.co/api/v2/pokemon-form/${i}`)} resourceName={"forms"}>
-                      <PokemonsForms key={pokemons.results[i].name}/>
-                    </DataSource>
+            <section className="section">
+                <div className="search">
+                <input type="text" onChange={(e) => setSearchByName(e.target.value)} placeholder="Search by name"/>
+                <button onClick={search}>search</button>
+                <button onClick={() => setSearchValue(null)}>reset</button>
+                </div>
 
-            ))}
+
+                {searchValue !== null ? (
+                        <>
+                            <div className="pokemons-grid">
+                                {searchValue.map((item, i) => (
+                                    <DataSource key={searchValue[i].name}
+                                                getDataFunc={getServerData(`https://pokeapi.co/api/v2/pokemon-form/${searchValue[i].name}`)}
+                                                resourceName={"forms"}>
+                                        <PokemonsForms key={searchValue[i].name}/>
+                                    </DataSource>
+
+                                ))}
+                            </div>
+
+                        </>)
+
+                    :
+
+                    (
+                        <>
+                            <div className="pokemons-grid">
+                                {pokemons.results.map((item, i) => (
+                                    <DataSource key={pokemons.results[i].name}
+                                                getDataFunc={getServerData(`https://pokeapi.co/api/v2/pokemon-form/${i}`)}
+                                                resourceName={"forms"}>
+                                        <PokemonsForms key={pokemons.results[i].name}/>
+                                    </DataSource>
+
+                                ))}
+                            </div>
+                        </>)
+
+                }
+
+
+            </section>
+
+
         </>) : <p>loading</p>
 
 
