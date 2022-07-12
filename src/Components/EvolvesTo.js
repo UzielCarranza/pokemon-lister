2f9a7df27d6fce20cc01f8bcecd62a0c94787842
UzielCarranza <uziel.carranza-charro@pgip-tech.com>
2022-07-12 16:22:54 -0500

    added image for pokemon's next forn

--- src/Components/EvolvesTo.js
+++ src/Components/EvolvesTo.js
@@ -1,47 +1,45 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
 
+export const EvolvesTo = ({url, pokemonName}) => {
+    const [selectedPokemon, setSelectedPokemon] = useState(null);
+    const [firstEvolution, setFirstEvolution] = useState(null)
+    const [data, setData] = useState(null);
+    const [secondForm, setSecondForm] = useState(null);
+    useEffect(() => {
+        if (pokemonName !== null) {
+            setSelectedPokemon(pokemonName)
+            axios.get(`${url}`)
+                .then(res => {
+                    setFirstEvolution(res.data)
 
-export const EvolvesTo = (props) => {
-    const [evolvesTo, setEvolvesTo] = useState(null);
-    const [nextForms, setNextForm] = useState(null)
-
+                })
+        }
+    }, [pokemonName, url])
 
     useEffect(() => {
-        if (props.evolves_To !== null) {
-            setEvolvesTo(props.evolves_To)
+        if (firstEvolution !== null) {
+            if (selectedPokemon === firstEvolution.chain.species.name) {
+                axios.get(`https://pokeapi.co/api/v2/pokemon-form/${firstEvolution.chain.evolves_to[0].species.name}`)
+                    .then(res => {
+                        setData(res.data)
+                    })
+                if (data !== null) {
+                    setSecondForm({
+                        name: firstEvolution.chain.evolves_to[0].species.name,
+                        img: data.sprites.front_default
+                    })
+                }
+
+            }
         }
-    }, [props.evolves_To])
-    // console.log(evolvesTo)
+    }, [data, firstEvolution, selectedPokemon])
 
-
-    useEffect(() => {
-        if (evolvesTo !== null){
-            // setEvolvesTo(evolvesTo.species.name)
-            setNextForm(evolvesTo.chain.evolves_to[0].species.name)
-            console.log(nextForms)
-
-        }
-
-    }, [evolvesTo, nextForms])
-    return <p>loading</p>
-
-    // useEffect(() => {
-    //     if (evolvesTo !== null) {
-    //         axios.get(`"https://pokeapi.co/api/v2/pokemon-species/${evolvesFrom.name}/`)
-    //             .then(res => {
-    //                 setPreviousForm(res.data)
-    //             })
-    //     }
-    // }, [evolvesFrom])
-    //
-    // return previousForms !== null ? (
-    //         <div>
-    //             <p>Evolves from</p>
-    //             <img src={previousForms.sprites.front_default} alt="previous-pokemons-forms"/>
-    //
-    //         </div>
-    //     )
-    //
-    //     : <p>loading</p>
+    return secondForm !== null ? (
+        <div>
+            <p>Next Evolution:</p>
+            <p>{secondForm.name}</p>
+            <img src={secondForm.img} alt="next-form"/>
+        </div>
+    ) : ' '
 }
