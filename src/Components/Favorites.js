e2a7902f491f383fce8e0129293f6d9b26dca9aa
UzielCarranza <uzielcarranzac97@gmail.com>
2022-07-19 15:33:46 -0400

    create component to render favorite pokemons

--- src/Components/Favorites.js
+++ src/Components/Favorites.js
@@ -1,8 +1,30 @@
 import {getFromLocalStorage} from "../Utils/getFromLocalStorage";
+import {DataSource} from "../Utils/DataSource";
+import {getServerData} from "../Utils/GetServerData";
+import {PokemonsForms} from "./PokemonsForms";
+import {Link, NavLink} from "react-router-dom";
 
 export const Favorites = () => {
     let fav = getFromLocalStorage();
+
     return fav ? (
-        <div><p>{fav[2]}</p></div>
+        <>
+            <div className="search">
+
+                <button>
+                    <NavLink style={{padding: 12, textDecoration: 'none'}} to="/"> All pokemons </NavLink>
+                </button>
+            </div>
+            <div className="pokemons-grid">
+                {fav.map((item, i) => (
+                    <DataSource key={fav[i]}
+                                getDataFunc={getServerData(`https://pokeapi.co/api/v2/pokemon-form/${fav[i]}`)}
+                                resourceName={"forms"}>
+                        <PokemonsForms/>
+                    </DataSource>
+
+                ))}
+            </div>
+        </>
     ) : <p>none</p>
 }
