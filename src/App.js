4142bf8786bd56e9879b736ef9580a9d36bd5a6d
UzielCarranza <uzielcarranzac97@gmail.com>
2022-07-19 15:32:48 -0400

    added routing to the project

--- src/App.js
+++ src/App.js
@@ -2,14 +2,31 @@ import {Pokemons} from "./Components/Pokemons";
 import {getServerData} from "./Utils/GetServerData";
 import {DataSource} from "./Utils/DataSource";
 
+import React from "react";
+import {Routes, Route} from "react-router-dom";
+import {Favorites} from "./Components/Favorites";
 
 function App() {
     return (
-        <>
-            <DataSource getDataFunc={getServerData('https://pokeapi.co/api/v2/pokemon/')} resourceName="pokemon" >
-                <Pokemons/>
-            </DataSource>
-        </>
+        <Routes>
+            <Route path="/" element={
+                <DataSource getDataFunc={getServerData('https://pokeapi.co/api/v2/pokemon/')} resourceName="pokemon">
+                    <Pokemons/>
+                </DataSource>
+            }/>
+            <Route path="/favorites" element={
+                <Favorites/>
+            }/>
+
+            <Route
+                path="*"
+                element={
+                    <main style={{padding: "1rem"}}>
+                        <p>There's nothing here!</p>
+                    </main>
+                }
+            />
+        </Routes>
     )
 }
 
