3f3ee90c31a5b8ffe366bea917e0c2d081d15fe5
UzielCarranza <uzielcarranzac97@gmail.com>
2023-01-14 14:19:58 -0500

    separated to favorites logic into its own component

--- src/Components/PokemonsForms.js
+++ src/Components/PokemonsForms.js
@@ -3,48 +3,26 @@ import {DataSource} from "../Utils/DataSource";
 import {getServerData} from "../Utils/GetServerData";
 import {Stats} from "./Stats";
 import {Modal} from "../Utils/Modal";
-import {AiTwotoneStar} from "react-icons/ai";
-import {toLocalStorage} from "../Utils/toLocalStorage";
-import {getFromLocalStorage} from "../Utils/getFromLocalStorage";
-import {removeFromLocalStorage} from "../Utils/removeFromLocalStorage";
 import {DisplayPokemonsHabitat} from "./DisplayPokemonsHabitat";
+import {IsAddedToFavorites} from "./functionality/IsAddedToFavorites";
 
 export const PokemonsForms = (props) => {
     const [forms, setForms] = useState(null);
-    const [favorite, setFavorite] = useState(false);
-
 
     useEffect(() => {
         if (props.forms !== null) {
-            setForms(props.forms)
-            let fav = getFromLocalStorage();
-            for (let i = 0; i <= fav.length; i++) {
-                if (fav[i] === props.forms.pokemon.name) {
-                    setFavorite(true)
-                }
-            }
+            setForms(props.forms);
 
         }
     }, [props])
 
-    const localStorageTrigger = () => {
-        setFavorite(true)
-        toLocalStorage(props.forms.pokemon.name);
-    }
-    const removeFavorite = () => {
-        setFavorite(false)
-        removeFromLocalStorage(props.forms.pokemon.name)
-    }
 
-    return forms !== null ? (<>
+    return forms !== null && (<>
 
 
         <div className="card">
-            {favorite === true ?
-                <AiTwotoneStar className='selected-favorite' style={{color: "yellow"}} onClick={removeFavorite}/>
-                :
-                <AiTwotoneStar className='unselected-favorite' style={{color: '#fff'}} onClick={localStorageTrigger}/>
-            }
+            <IsAddedToFavorites pokemon={props.forms} />
+
             <span style={{fontSize: 20, fontWeight: "bold", marginLeft: 20}}>{forms.types[0].type.name}</span>
             <h1 className="pokemon-name">{props.forms.pokemon.name}</h1>
             <div className="img-section-wrapper">
@@ -62,5 +40,5 @@ export const PokemonsForms = (props) => {
         </div>
 
 
-    </>) : ""
+    </>)
 }
